aa319679232eab27a1d14819c2d894c70aeb074e
BENNY BABY <80648724+BennyBaby23@users.noreply.github.com>
2022-12-14 19:24:56 -0500

    install and setup everything

---
+++ config/globals.js
@@ -0,0 +1,9 @@
+ //config for mongodb connecting to database
+const configuration = {
+    'db': 'mongodb+srv://admin:lD1tWJd230ochjyQ@cluster0.ndlnbb0.mongodb.net/COMP3033FinalExam'
+}
+
+module.exports = configuration;
+
+
+
