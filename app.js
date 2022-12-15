b417b40e1ced46dc7099709669efe10112ec4f4a
BENNY BABY <80648724+BennyBaby23@users.noreply.github.com>
2022-12-14 19:42:00 -0500

    connected db in app.js and created modules

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+const mongoose = require('mongoose');
+const config = require('./config/globals');
 
 var indexRouter = require('./routes/index');
 
@@ -21,6 +23,11 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 
+//connect mongoose mongodb
+mongoose.connect(config.db, {useNewUrlParser: true, useUnifiedTopology: true })
+.then((message) => { console.log('restaurant connected  successfully');})
+.catch((error)=>{ console.log('Sorry error while connecting ${error}');
+});
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
---
+++ models/restaurant.js
@@ -0,0 +1,22 @@
+//Restaurant model class
+
+//require imports interface of mongodb
+const mongoose = require('mongoose');
+const express = require('express');
+
+//object’s properties variable schema
+const schemaDefinition = {
+
+    name: { type: String, required: true },
+  address: { type: String, required: true },
+  phoneNumber: { type: Number, required: true },
+  emailAddress: { type: String},
+  rating: { type: Number, max: 10, required: true }
+  
+}
+
+//schema obj to export
+let schemaObj = new mongoose.Schema(schemaDefinition);
+
+
+module.exports = mongoose.model('restaurant', schemaObj);
